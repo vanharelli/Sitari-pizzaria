6db717898248167d474f47c8554d012698052e9f
Seu Nome <seu_email_do_github@gmail.com>
2026-05-17 13:31:44 -0300

    Infra: index.css mobile armor + tailwind config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles/core.css";
+import "./index.css";
 import "./styles/animations_led.css";
 import { initLockdown } from "./logic/lockdown";
 
